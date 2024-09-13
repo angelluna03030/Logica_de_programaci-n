f33ea2690ef17562bf83b3cab58679b8fb0eadca
angel luna <angelstivengarcialuna@gmail.com>
2024-09-12 23:23:31 -0500

    Primer ejercicio

---
+++ index.py
@@ -0,0 +1,25 @@
+
+def juegetesbusqueda(jugetesid):
+    lent = len(jugetesid)
+    repetir = -1
+    for i in range(lent):
+        for j in range(i + 1, lent):
+            a = jugetesid[i]
+            b = jugetesid[j]
+        if a == b:
+            repetir = a
+            lent = j 
+            
+            break
+
+jugetesid = [1,2,3,4,45,2,4,90,7,8,9,0]
+
+
+resultado = juegetesbusqueda(jugetesid)
+print(resultado)
+            
+        
+
+
+
+
